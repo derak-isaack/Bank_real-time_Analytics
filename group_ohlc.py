55a6d97d64e2986ce7bfeac5faa92d5e0be626c6
deerak-isaack <oderaisaack@gmail.com>
2024-07-11 22:29:51 +0300

    script to open sql file

---
+++ group_ohlc.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="@admin#2024*10",
+    database="Minute_batches"
+)
+cursor = conn.cursor()
+
+with open("ohlc_groups.sql", 'r') as table:
+    mysql_table = table.read()
+    
+for statement in mysql_table.split(';'):
+        statement = statement.strip()
+        if statement:
+            cursor.execute(statement)
+            
+cursor.commit()
+   
+#Close the connection to avoid errors when writing data to postgres     
+cursor.close()
+    
+    
+
+    
+    
