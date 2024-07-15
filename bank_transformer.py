b5645f75abecb80557f9cc83f160cdbdaa3ae4db
deerak-isaack <oderaisaack@gmail.com>
2024-07-15 10:03:27 +0300

    added function to send data to mysql

--- bank_transformer.py
+++ bank_transformer.py
@@ -1,33 +1,47 @@
 from quixstreams import Application
 import logging
-import json
+import json, mysql.connector
 from datetime import timedelta
 
 
 #Define functions for grouping transactions on a 5 minute interval
 def initialize_fn(msg):
-
-    amount = msg['transAmount']
+    amounts = [float(transaction['transAmount']) for transaction in msg]
     
-    return{
-        "open": amount,
-        "high": amount,
-        "low": amount,
-        "close": amount,
+    if not amounts:
+        return {"open": None, "high": None, "low": None, "close": None}
+
+    return {
+        "open": amounts[0],
+        "high": max(amounts),
+        "low": min(amounts),
+        "close": amounts[-1],
     }
     
 def reducer_fn(summary, msg):
-
-    amount = msg['transAmount']
+    if isinstance(msg, list):
+        amounts = float(msg[0]['transAmount'])  
+    else:
+        amounts = float(msg['transAmount'])
+    
 
     return{
         "open": summary["open"],
-        "high": max(summary["high"], amount),
-        "low": min(summary["low"], amount),
-        "close": amount,
+        "high": max(summary["high"], amounts),
+        "low": min(summary["low"], amounts),
+        "close": amounts,
     }
 
-
+def send_to_mysql(summary, connection):
+        cursor = connection.cursor()
+        print("Sending to MySQL...")
+        # Code to send data to MySQL database goes here
+        query = """INSERT INTO transaction_summary (start, end, open_transaction, high_transaction, low_transaction, close_transaction) VALUES (%s, %s, %s, %s, %s, %s)"""
+        values = (summary['start'], summary['end'], summary['value']['open'], summary['value']['high'], summary['value']['low'], summary['value']['close'])
+        cursor.execute(query, values)
+        connection.commit()
+        cursor.close()
+        print("Data sent to MySQL successfully!")
 
 def main():
     app = Application(
@@ -36,30 +50,30 @@ def main():
         auto_offset_reset="latest",
         consumer_group="BankStreamingFake"
     )
+    # app.clear_state()
     
     input_topic = app.topic("BankStreaming")
+    output_topic = app.topic("BankStreamingSummary")
     
     sdf = app.dataframe(topic=input_topic)
+    # sdf = sdf.update(lambda row: print(row))
     sdf = (
-    sdf.tumbling_window(timedelta(minutes=5))
+    sdf.tumbling_window(timedelta(seconds=10))
     .reduce(reducer=reducer_fn, initializer=initialize_fn)
+    ).final()
     
-    .final()
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="@admin#2024*10",
+        database="minute_batches"
     )
+    sdf = sdf.update(lambda msg: logging.debug(print(msg)))
+    sdf = sdf.update(lambda msg: send_to_mysql(msg, connection))
+    # sdf = sdf.to_topic(output_topic)
+    #sdf = sdf.apply(send_to_mysql)
     app.run(sdf)
     
-    # with app.get_consumer() as consumer:
-    #     consumer.subscribe(["BankStreaming"])
-    #     while True:
-    #         msg = consumer.poll(2)
-            
-    #         if msg is None:
-    #             print("waiting for transaction...")
-    #         else:
-    #             key = msg.key().decode("utf-8")
-    #             value = json.loads(msg.value())
-                
-    #             print(f"Received transaction: {type(value)}")
                 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
