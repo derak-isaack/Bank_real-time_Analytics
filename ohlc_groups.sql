cc3623be2c7358a6a293cf67b74408f69ab86f2b
deerak-isaack <oderaisaack@gmail.com>
2024-07-16 17:12:40 +0300

    added datetime column as a timestamp

--- ohlc_groups.sql
+++ ohlc_groups.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS transaction_summary (
+                date_time TIMESTAMP NOT NULL,
                 start BIGINT NOT NULL,
                 end BIGINT NOT NULL,
                 open_transaction FLOAT NOT NULL,
